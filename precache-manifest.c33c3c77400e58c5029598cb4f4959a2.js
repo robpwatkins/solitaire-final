9904410419ccf9fdb735ebc77519db3569c2e862
Rob W <robpwatkins@gmail.com>
2020-06-07 12:05:18 -0500

    Updates

---
+++ precache-manifest.c33c3c77400e58c5029598cb4f4959a2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4658f5432abd97c3ad8c849c611ef977",
+    "url": "/solitaire-final/index.html"
+  },
+  {
+    "revision": "0def592f2939c3c0d76d",
+    "url": "/solitaire-final/static/css/main.68c8282f.chunk.css"
+  },
+  {
+    "revision": "63c017b3d7e37cd35190",
+    "url": "/solitaire-final/static/js/2.ea3260f0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/solitaire-final/static/js/2.ea3260f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0def592f2939c3c0d76d",
+    "url": "/solitaire-final/static/js/main.b8022e44.chunk.js"
+  },
+  {
+    "revision": "15df14bc5818a2397da3",
+    "url": "/solitaire-final/static/js/runtime-main.6b9266d8.js"
+  },
+  {
+    "revision": "a75b7c4d5087d1c01ff51ad6f2ab9911",
+    "url": "/solitaire-final/static/media/Clubs.a75b7c4d.png"
+  },
+  {
+    "revision": "3109c0531e8db9b95c0a3d40d59469ad",
+    "url": "/solitaire-final/static/media/Heartsss.3109c053.png"
+  }
+]);
